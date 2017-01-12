9f7688b0219b9087a71a026c57390327a2e0d076
iainm <iain.millar@rightmove.co.uk>
2017-01-12 11:02:26 +0000

    EMU Poland first commit.

---
+++ Even_MOAR_Units_Poland/Data/Even_MOAR_Units_Poland_Data.sql
@@ -0,0 +1,19 @@
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_SWORDSMAN';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_SPEARMAN';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_PIKEMAN';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_HEAVY_CHARIOT';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_KNIGHT';
