80ae10b7d152e3730531ffd06eaa3ed5e20f82d0
iainm <iain.millar@rightmove.co.uk>
2017-01-13 16:41:06 +0000

    EMU Poland gameplay.

--- Even_MOAR_Units_Poland/Data/Even_MOAR_Units_Poland_Data.sql
+++ Even_MOAR_Units_Poland/Data/Even_MOAR_Units_Poland_Data.sql
@@ -1,3 +1,4 @@
+/*
 UPDATE Units
 SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100'
 WHERE  UnitType = 'UNIT_SWORDSMAN';
@@ -16,4 +17,4 @@ WHERE  UnitType = 'UNIT_HEAVY_CHARIOT';
 
 UPDATE Units
 SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
-WHERE  UnitType = 'UNIT_KNIGHT';
+WHERE  UnitType = 'UNIT_KNIGHT';*/
