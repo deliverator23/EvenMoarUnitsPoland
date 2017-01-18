87e959faeaeaa6d1816072d9580487f67a59a552
iainm <iain.millar@rightmove.co.uk>
2017-01-18 09:44:22 +0000

    Reset SQL.

--- Even_MOAR_Units_Poland/Data/Even_MOAR_Units_Poland_Data.sql
+++ Even_MOAR_Units_Poland/Data/Even_MOAR_Units_Poland_Data.sql
@@ -1,3 +1,4 @@
+/*
 UPDATE Units
 SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
 WHERE  UnitType = 'UNIT_CHOSEN_INFANTRY';
@@ -5,6 +6,4 @@ WHERE  UnitType = 'UNIT_CHOSEN_INFANTRY';
 UPDATE Units
 SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
 WHERE  UnitType = 'UNIT_UHLAN';
-
-/*
 */
