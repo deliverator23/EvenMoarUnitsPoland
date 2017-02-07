9b6bba0d45d2fbfb487ace4464bd947757f311d2
iainm <iain.millar@rightmove.co.uk>
2017-02-07 14:48:27 +0000

    Added Janissaries for Preslav and Yerevan Suzerain.

--- Even_MOAR_Units_Poland/Data/Even_MOAR_Units_Poland_Data.sql
+++ Even_MOAR_Units_Poland/Data/Even_MOAR_Units_Poland_Data.sql
@@ -6,4 +6,6 @@ WHERE  UnitType = 'UNIT_CHOSEN_INFANTRY';
 UPDATE Units
 SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
 WHERE  UnitType = 'UNIT_UHLAN';
+
+Winged Hussar Fix
 */
